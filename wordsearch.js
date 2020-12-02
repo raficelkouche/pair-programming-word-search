75614fa85f8ef77ddc9aa7f51566624d719e5ae0
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-02 18:12:41 -0500

    fixed the function to check for word matches both horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters.length === 0) { return undefined};
+
+    let horizontal = letters.map(ls => ls.join(''));
+    if (horizontal.includes(word)) {
+        return true;
     }
+    //Check if its there vertically
+    let j = 0 //keeps track of columns
+    while (j < letters[0].length) {
+        const col = [];
+        for (let row = 0; row < letters.length; row++) {
+            col.push(letters[row][j]);
+        }
+        //[a,s,y,h,w,b,u,o,e]
+        let vertical = col.join(''); //[asyhwbuoe]
+        if (vertical.includes(word)) {
+            return true
+        }
+        else {
+            j++
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
+
